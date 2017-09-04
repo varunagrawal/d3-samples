f0898867ed6066092db8cdb1cad512285b6ecda1
Varun Agrawal <varagrawal@gmail.com>
2017-09-04 16:51:48 -0400

    improvements to wordcloud

--- wordcloud/wordcloud.js
+++ wordcloud/wordcloud.js
@@ -1,12 +1,12 @@
-windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+windowWidth = 1000; //window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+windowHeight = 1000; //window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 var margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
 width = windowWidth - margin.left - margin.right;
 height = windowHeight - margin.bottom - margin.top;
 
-let stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
+let stopwords = ["a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "use", "used", "users", "work", "even", "via", "need", "will", "thus", "rnns", "found", "able", "one", "can", "using", "often", "models", "networks", "cnns", "tests", "given"];
 
 
 function getWordCounts(data) {
@@ -29,7 +29,7 @@ function getWordCounts(data) {
         arr.push({ text: key, count: counts[key] });
     }
 
-    var res = arr.filter(function(x) { return x.count > 1 && x.count < 20; });
+    var res = arr.filter(function(x) { return x.count > 2 && x.count <= 35; });
     return res;
 }
 
@@ -38,12 +38,15 @@ var fill = d3.scaleOrdinal(d3.schemeCategory10);
 var xScale = d3.scaleLinear()
     .range([0, width]);
 
+svg_width = 700;
+svg_height = 700;
+
 var svg = d3.select("body")
     .append("svg")
-    .attr("width", "500")
-    .attr("height", "500");
+    .attr("width", svg_width)
+    .attr("height", svg_height);
 
-var chartGroup = svg.append("g").attr("transform", "translate(250,250)");
+var chartGroup = svg.append("g").attr("transform", "translate(" + svg_width/2 + "," + svg_height/2 + ")");
 
 // Make the word cloud
 
@@ -63,7 +66,7 @@ function drawCloud(words) {
 }
 
 console.log("Loading data");
-d3.json("corpus.json", function(error, data) {
+d3.json("mlatgt_posters.json", function(error, data){
     if (error) throw error;
 
     var text = data.text;
@@ -71,15 +74,18 @@ d3.json("corpus.json", function(error, data) {
     console.log("calculating stats");
     word_counts = getWordCounts(text);
 
+    console.log("Adversarial", word_counts.filter(function(x) { return x.text == "adversarial"}));
+
     // document.getElementById("corpus").innerHTML = word_counts.map(function(x) { return x.text });
-    console.log(word_counts.map(function(x) { return x.text }));
+    // console.log(word_counts.map(function(x) { return x.text }));
+
+    let is_rotated = 1;
 
     var wordcloud = d3.layout.cloud()
-        .size([500, 500])
-        // .words(word_counts)
+        .size([svg_width, svg_height])
         .words(word_counts)
         .padding(2)
-        .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .rotate(function() { return is_rotated * (~~(Math.random() * 2) * 90); })
         .fontSize(function(d) { return d.count * 8; })
         .on("end", drawCloud) // register a callback to insert the elements into the group once the layout has been finalized
         .start();
